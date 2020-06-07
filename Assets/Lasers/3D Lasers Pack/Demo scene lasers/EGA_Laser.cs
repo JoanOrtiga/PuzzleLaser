2c0a23d4576ad8d401121cfbe97b769cde24bec0
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-06-07 02:08:18 +0200

    LaserWork

--- Assets/Lasers/3D Lasers Pack/Demo scene lasers/EGA_Laser.cs
+++ Assets/Lasers/3D Lasers Pack/Demo scene lasers/EGA_Laser.cs
@@ -24,7 +24,6 @@ public class EGA_Laser : MonoBehaviour
     private ParticleSystem[] Hit;
 
     private CheckLaserActive currentCube;
-    //EGA_Laser reflectedLaser = null;
 
 
     BoxCollider boxColl;
@@ -40,117 +39,76 @@ public class EGA_Laser : MonoBehaviour
         Laser.useWorldSpace = true;
     }
 
+
+    public int reflections;
+    private Ray ray;
+    private RaycastHit hit;
+    private Vector3 direction;
+
     void Update()
     {
-        Laser.material.SetTextureScale("_MainTex", new Vector2(Length[0], Length[1]));                    
+        // boxColl.size = new Vector3(boxColl.size.x, boxColl.size.y, Vector3.Distance(transform.position, hit.point));
+        // boxColl.center = new Vector3(boxColl.center.x, boxColl.center.y, Vector3.Distance(transform.position, hit.point)/2);
+
+
+        Laser.material.SetTextureScale("_MainTex", new Vector2(Length[0], Length[1]));
         Laser.material.SetTextureScale("_Noise", new Vector2(Length[2], Length[3]));
-        //To set LineRender position
+
+        ray = new Ray(transform.position, transform.forward);
+
+        Laser.positionCount = 1;
+        Laser.SetPosition(0, transform.position);
+        float remainingLength = MaxLength;
+
         if (Laser != null && UpdateSaver == false)
         {
-            Laser.SetPosition(0, transform.position);
-            RaycastHit hit; 
-       
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, MaxLength))
+            for (int i = 0; i < reflections; i++)
             {
-               
-
-                //End laser position if collides with object
-                Laser.SetPosition(1, hit.point);
-                HitEffect.transform.position = hit.point + hit.normal * HitOffset;
-                //Hit effect zero rotation
-                HitEffect.transform.rotation = Quaternion.identity;
-                foreach (var AllPs in Effects)
+                if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
                 {
-                    if (!AllPs.isPlaying) AllPs.Play();
+                    Laser.positionCount += 1;
+                    Laser.SetPosition(Laser.positionCount - 1, hit.point);
+                    remainingLength -= Vector3.Distance(ray.origin, hit.point);
+                    ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+
+                    Length[0] = MainTextureLength * (Vector3.Distance(transform.position, hit.point));
+                    Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, hit.point));
+
+                    if(hit.collider.tag == "Activable")
+                    {
+                        hit.collider.GetComponent<CheckLaserActive>().laserHit = true;
+                    }
+
+                    if (hit.collider.tag != "Mirror")
+                    {
+                        HitEffect.transform.position = hit.point + hit.normal * HitOffset;
+                        //Hit effect zero rotation
+                        HitEffect.transform.rotation = Quaternion.identity;
+                        foreach (var AllPs in Effects)
+                        {
+                            if (!AllPs.isPlaying) AllPs.Play();
+                        }
+                        break;
+                    }
+
                 }
-                //Texture tiling
-                Length[0] = MainTextureLength * (Vector3.Distance(transform.position, hit.point));
-                Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, hit.point));
-                //Check if hit reflexion
-                //CheckReflexion(hit);
-                //Check if hit is an end
-                CheckActivate(hit);
-            }
-            else
-            {
-                //End laser position if doesn't collide with object
-                var EndPos = transform.position + transform.forward * MaxLength;
-                Laser.SetPosition(1, EndPos);
-                HitEffect.transform.position = EndPos;
-                foreach (var AllPs in Hit)
+                else
                 {
-                    if (AllPs.isPlaying) AllPs.Stop();
+                    Laser.positionCount += 1;
+                    Laser.SetPosition(Laser.positionCount - 1, ray.origin + ray.direction * remainingLength);
+
+                    foreach (var AllPs in Hit)
+                    {
+                        if (AllPs.isPlaying) AllPs.Stop();
+                    }
+
                 }
-                //Texture tiling
-                Length[0] = MainTextureLength * (Vector3.Distance(transform.position, EndPos));
-                Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, EndPos));
             }
 
-            //Insurance against the appearance of a laser in the center of coordinates!
             if (Laser.enabled == false && LaserSaver == false)
             {
                 LaserSaver = true;
                 Laser.enabled = true;
-            }
-
-            boxColl.size = new Vector3(boxColl.size.x, boxColl.size.y, Vector3.Distance(transform.position, hit.point));
-            boxColl.center = new Vector3(boxColl.center.x, boxColl.center.y, Vector3.Distance(transform.position, hit.point)/2);
-
-            print(hit.collider.transform.tag);
-        }  
-    }
-
-
-    /*
-    private void CheckReflexion(RaycastHit hit)
-    {
-        if (hit.collider.CompareTag("ReflexionCube"))
-        {
-            if (reflectedLaser == null)
-            {
-                reflectedLaser = Instantiate(this, hit.point + hit.normal * HitOffset, hit.collider.transform.rotation);
-            }
-        }
-        else if (reflectedLaser != null)
-        {
-            Destroy(reflectedLaser);
-        }
-    }
-    */
-    private void CheckActivate(RaycastHit hit)
-    {
-        CheckLaserActive finalCube = hit.collider.GetComponent<CheckLaserActive>();
-        if (finalCube != null)
-        {
-            if (currentCube == null)
-            {
-                currentCube = finalCube;
-            }
-            if (currentCube == finalCube)
-            {
-                finalCube.laserHit = true;
-            }
-        }
-        else if (currentCube != null)
-        {
-            currentCube.laserHit = false;
-            currentCube = null;
-        }
-    }
-
-    public void DisablePrepare()
-    {
-        if (Laser != null)
-        {
-            Laser.enabled = false;
-        }
-        UpdateSaver = true;
-        //Effects can = null in multiply shooting
-        if (Effects != null)
-        {
-            foreach (var AllPs in Effects)
-            {
-                if (AllPs.isPlaying) AllPs.Stop();
             }
         }
     }
